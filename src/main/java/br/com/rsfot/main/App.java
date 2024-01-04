720cc040d9cb4d9fff47fe1002c27d88fe46d927
Rui Nascimento <sruy19@gmail.com>
2024-01-03 21:28:45 -0300

    feat: file to do manual test

---
+++ src/main/java/br/com/rsfot/main/App.java
@@ -0,0 +1,59 @@
+package br.com.rsfot.main;
+
+import br.com.rsfot.game.HuntWumpus;
+
+import java.util.Scanner;
+
+import static br.com.rsfot.domain.Rotation.LEFT;
+import static br.com.rsfot.domain.Rotation.RIGHT;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        HuntWumpus huntWumpus = new HuntWumpus();
+
+        boolean exit = false;
+        while (!exit) {
+            huntWumpus.getEnvironment().showCave();
+            System.out.println(menu());
+            System.out.println(huntWumpus.getAgent());
+            System.out.println(huntWumpus.getEnvironment().getFeelingsByCoordinate());
+            int option = Integer.parseInt(scanner.nextLine());
+            switch (option) {
+                case 1:
+                    huntWumpus.turnAgentTo(LEFT);
+                    break;
+                case 2:
+                    huntWumpus.turnAgentTo(RIGHT);
+                    break;
+                case 3:
+                    huntWumpus.moveForward();
+                    break;
+                case 4:
+                    huntWumpus.grabGold();
+                    break;
+                case 5:
+                    huntWumpus.shoot();
+                    break;
+                case 6:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
+
+    }
+
+    public static String menu() {
+        return """
+                1 - Turn left
+                2 - Turn right
+                3 - Move forward
+                4 - Grab Gold
+                5 - Shoot
+                6 - Exit
+                """;
+    }
+}
